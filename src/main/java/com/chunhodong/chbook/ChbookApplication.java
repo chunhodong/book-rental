afddd41fbc8af07a50555848eb3114fb38254289
chunhodong <yhms4432@naver.com>
2023-02-04 23:03:44 +0900

    init

---
+++ src/main/java/com/chunhodong/chbook/ChbookApplication.java
@@ -0,0 +1,13 @@
+package com.chunhodong.chbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chunhodong/chbook/ChbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunhodong.chbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
